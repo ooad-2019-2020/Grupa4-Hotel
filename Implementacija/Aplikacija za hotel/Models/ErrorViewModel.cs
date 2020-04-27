0f3ad39df68466694f6828817fc8240c5bded9cb
Velid Poško <vposko1@etf.unsa.ba>
2020-04-27 23:31:15 +0200

    Prvi commit

---
+++ Implementacija/Aplikacija za hotel/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Aplikacija_za_hotel.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
